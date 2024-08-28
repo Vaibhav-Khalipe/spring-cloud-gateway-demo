66efff97efbbbd46cb5dd7bfa62c6d52e04b6ed9
Khalipe <VJK1871997@gmail.com>
2024-08-28 16:29:33 +0530

    Api gateWay

---
+++ src/main/java/com/prowings/springcloudgatewaydemo/SpringCloudGatewayDemoApplication.java
@@ -0,0 +1,15 @@
+package com.prowings.springcloudgatewaydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SpringCloudGatewayDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudGatewayDemoApplication.class, args);
+	}
+
+}
